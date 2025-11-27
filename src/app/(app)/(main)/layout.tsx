61ae654540cc643b0b9193ec9ff4dbaa68c772e0
HTLA380 <htetlin099798@gmail.com>
2025-11-27 21:08:43 +0630

    fix: fix duplicated footer by organizing the route groups

--- src/app/(app)/(main)/layout.tsx
+++ src/app/(app)/(main)/layout.tsx
@@ -1,16 +1,13 @@
 import type React from "react";
 import { Footer } from "@/components/footer";
-import Header from "@/components/header";
 
-const RootLayout = ({ children }: { children: React.ReactNode }) => (
+const MainLayout = ({ children }: { children: React.ReactNode }) => (
   <>
-    <Header />
     {children}
-
     <div className="flex w-full justify-center px-6 pb-8">
       <Footer className="max-w-4xl px-4 lg:px-6" />
     </div>
   </>
 );
 
-export default RootLayout;
+export default MainLayout;
