2afa6ee44dba073ff1fbe6d29bab4fe4588a3758
HTLA380 <htetlin099798@gmail.com>
2025-11-27 16:22:17 +0630

    chore: restructure layout

--- src/app/(root)/(blog)/layout.tsx
+++ src/app/(root)/(blog)/layout.tsx
@@ -1,12 +1,8 @@
 import { RootProvider } from "fumadocs-ui/provider/next";
 import type React from "react";
-import Header from "@/components/header";
 
 const BlogLayout = ({ children }: { children: React.ReactNode }) => (
-  <RootProvider>
-    <Header />
-    {children}
-  </RootProvider>
+  <RootProvider>{children}</RootProvider>
 );
 
 export default BlogLayout;
