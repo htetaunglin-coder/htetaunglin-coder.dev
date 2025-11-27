2f050a247b0f43ba5152df8217e2b2bf79905548
HTLA380 <htetlin099798@gmail.com>
2025-11-27 21:14:16 +0630

    chore: add reminder comment

--- src/app/(app)/(main)/about/page.tsx
+++ src/app/(app)/(main)/about/page.tsx
@@ -2,6 +2,9 @@ import { About } from "@/features/about/components/about";
 
 import { ProfileBadge3D } from "@/features/about/components/profile-badge";
 
+// We have to revalidate (AKA: update) the about me page every 7 days or so
+// so that we can display the current age correctly. I think there would be a
+// better solution to this but currently, I'll just leave it as it is.
 export const revalidate = 604_800; // 7 days
 
 const AboutPage = () => (
