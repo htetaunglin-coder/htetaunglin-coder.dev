f46c3121961442fdaae5ac9eb5369a739e511491
HTLA380 <htetlin099798@gmail.com>
2025-11-27 15:57:51 +0630

    fix: fix hydration issue

--- src/components/comment.tsx
+++ src/components/comment.tsx
@@ -2,9 +2,19 @@
 
 import Giscus from "@giscus/react";
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 const Comment = () => {
   const { theme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
 
   return (
     <Giscus
