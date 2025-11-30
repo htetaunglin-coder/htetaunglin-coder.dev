344e72c2d0feff558e6117697a488bb762e5494e
HTLA380 <htetlin099798@gmail.com>
2025-11-30 22:04:09 +0630

    chore: update blog

--- src/features/blog/components/mdx-components.tsx
+++ src/features/blog/components/mdx-components.tsx
@@ -1,8 +1,13 @@
+import {
+  Accordion as FumadocsAccordion,
+  Accordions as FumadocsAccordions,
+} from "fumadocs-ui/components/accordion";
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
 import { CloudinaryImage } from "@/components/cloudinary-image";
 import { NavLink } from "@/components/ui/nav-link";
 import { YoutubeIframe } from "@/components/youtube-iframe";
+import { cn } from "@/lib/utils";
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
@@ -11,5 +16,30 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
     CloudinaryImage,
     YoutubeIframe,
     a: NavLink,
+    Accordions,
+    Accordion,
   };
 }
+
+const Accordions = ({
+  className,
+  ...props
+}: React.ComponentProps<typeof FumadocsAccordions>) => (
+  <FumadocsAccordions
+    className={cn("border-none bg-transparent", className)}
+    {...props}
+  />
+);
+
+const Accordion = ({
+  className,
+  ...props
+}: React.ComponentProps<typeof FumadocsAccordion>) => (
+  <FumadocsAccordion
+    className={cn(
+      "[&_button]:[&_svg]:!size-4.5 rounded-none [&>div:first-of-type]:bg-bg-secondary [&_button]:flex-row-reverse [&_button]:justify-between [&_h3]:bg-bg-secondary/40",
+      className
+    )}
+    {...props}
+  />
+);
